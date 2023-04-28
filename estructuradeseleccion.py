cea3677741b93c4a6012ac858038e183ea96129c
Samahal111 <erick.toro.97@gmail.com>
2023-04-27 23:26:35 -0600

    Se agrega el proyecto

---
+++ estructuradeseleccion.py
@@ -0,0 +1,6 @@
+numero = int(input('ingrese cualquier nÃºmero: '))
+if numero > 32:
+    print("El nÃºmero " + str(numero) + " es mayor que el 32")
+else: 
+    print("El nÃºmero " + str(numero) + " es menor o igual que el 32")
+print("Esto se ve siempre")
